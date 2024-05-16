50a9bcda3b151ed28a2d62f11f1dd3be0712a959
mrst2000 <75087475+mrst2000@users.noreply.github.com>
2024-05-16 09:15:10 +0000

    Update config.go

--- transport/internet/websocket/config.go
+++ transport/internet/websocket/config.go
@@ -26,8 +26,7 @@ func (c *Config) GetRequestHeader() http.Header {
 		header.Add(k, v)
 	}
 	header.Set("hoSt", c.Host)
-	header.Set("User-Agent", "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36
-")
+	header.Set("User-Agent", "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36")
 	return header
 }
 
