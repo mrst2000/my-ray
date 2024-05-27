9d8fafbe2f2a38c758c2f3fa863c0741bc4b91ce
mrst2000 <75087475+mrst2000@users.noreply.github.com>
2024-05-27 20:17:49 +0000

    Update config.go

--- transport/internet/websocket/config.go
+++ transport/internet/websocket/config.go
@@ -31,8 +31,6 @@ func (c *Config) GetRequestHeader() http.Header {
 	randomizedHost := randomizeCase(c.Host)
 	header.Set("Host", randomizedHost)
 	header.Set("User-Agent", "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36")
-	header.Set("Connection", "uPGrAdE")
-	header.Set("Upgrade", "WEbsOcKeT")
 	return header
 }
 
