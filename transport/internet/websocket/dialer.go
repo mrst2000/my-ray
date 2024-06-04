8f487a9322df40f2dc1b5a8897346f15abd63d3c
mrst2000 <75087475+mrst2000@users.noreply.github.com>
2024-06-04 13:40:00 +0000

    Update dialer.go

--- transport/internet/websocket/dialer.go
+++ transport/internet/websocket/dialer.go
@@ -1,24 +1,24 @@
 package websocket
 
 import (
-    "bytes"
-    "context"
-    _ "embed"
-    "encoding/base64"
-    "io"
-    gonet "net"
-    "net/http"
-    "time"
+	"bytes"
+	"context"
+	_ "embed"
+	"encoding/base64"
+	"io"
+	gonet "net"
+	"net/http"
+	"time"
 
-    "github.com/gorilla/websocket"
-    "github.com/xtls/xray-core/common"
-    "github.com/xtls/xray-core/common/net"
-    "github.com/xtls/xray-core/common/platform"
-    "github.com/xtls/xray-core/common/session"
-    "github.com/xtls/xray-core/common/uuid"
-    "github.com/xtls/xray-core/transport/internet"
-    "github.com/xtls/xray-core/transport/internet/stat"
-    "github.com/xtls/xray-core/transport/internet/tls"
+	"github.com/gorilla/websocket"
+	"github.com/xtls/xray-core/common"
+	"github.com/xtls/xray-core/common/net"
+	"github.com/xtls/xray-core/common/platform"
+	"github.com/xtls/xray-core/common/session"
+	"github.com/xtls/xray-core/common/uuid"
+	"github.com/xtls/xray-core/transport/internet"
+	"github.com/xtls/xray-core/transport/internet/stat"
+	"github.com/xtls/xray-core/transport/internet/tls"
 )
 
 //go:embed dialer.html
